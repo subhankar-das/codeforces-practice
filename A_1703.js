ce47463fa242b9ceba2a5e139cf41820bffc7728
Subhankar Das <subhankardas695@gmail.com>
2026-01-14 03:16:42 +0000

    1703A

---
+++ A_1703.js
@@ -0,0 +1,11 @@
+// A. YES or YES?
+var n = Number(readline());
+
+while(n--){
+    var str = readline();
+    if(str.toLowerCase() === 'yes') {
+        print('YES')
+    }else {
+        print('NO')
+    }
+}
